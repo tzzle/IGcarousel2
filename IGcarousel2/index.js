5cbb362df399837fcb0f4b537e67f4ebde0251cb
tzzle <130739462+tzzle@users.noreply.github.com>
2023-05-31 13:11:36 +0300

    Add files via upload

---
+++ IGcarousel2/index.js
@@ -0,0 +1,9 @@
+function openDoor() {
+    document.getElementById("door1").style.display = 'none';
+    document.getElementById("door2").style.display = 'block';
+}
+
+function closeDoor() {
+    document.getElementById("door1").style.display = 'block';
+    document.getElementById("door2").style.display = 'none';
+}
